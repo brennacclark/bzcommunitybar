ef0d28412ba32b1c7cb1962df1f24b5b17315767
Brenna Clark <brennacclark@gmail.com>
2024-01-14 18:11:26 -0500

    Adding Hero Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/views/Header'
+import Hero from './components/views/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <div>
         <Header />
+        <Hero />
       </div>
 
     </>
---
+++ src/components/views/Hero.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+export default class Hero extends Component {
+  render() {
+    return (
+    // <!-- ### HERO CONTENT ### -->
+    <section className="bg-dark text-light p-5 text-center">
+      <div className="container">
+        {/* <!-- Centered Image --> */}
+        <img
+          className="img-fluid mx-auto d-block w-50"
+          src="src/assets/Bz_Sign.png"
+          alt="B'z Community Bar Logo"
+        />
+        <div className="d-sm-flex align-items-center justify-content-between">
+          <div>
+            <h2 className="mt-4">Mocktails, Cocktails, and Grub</h2>
+            <p className="my-4">Tuesday - Sunday | 5pm - 1am</p>
+            <hr />
+
+            <p className="my-4">
+              B'z Community Bar provides a safe space for the community to
+              engage in art, music, collaboration, and networking in the
+              creative capital.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    )
+  }
+}
