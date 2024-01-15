d3952ea8a4126f6d781a2acd5a96ecbca9214e94
Brenna Clark <brennacclark@gmail.com>
2024-01-14 20:02:53 -0500

    Removing old references

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
 import Header from "./components/views/Header";
 import Hero from "./components/views/Hero";
 import Menu from "./components/views/Menu";
@@ -8,21 +7,18 @@ import Events from "./components/views/Events";
 import Socials from "./components/views/Socials";
 import Footer from "./components/views/Footer";
 
-
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <BrowserRouter>
-          <Header />
-          <Hero />
-          <Menu />
-          <Events />
-          <Socials />
-          <Footer />
-        </BrowserRouter>
+        <Header />
+        <Hero />
+        <Menu />
+        <Events />
+        <Socials />
+        <Footer />
       </div>
     </>
   );
