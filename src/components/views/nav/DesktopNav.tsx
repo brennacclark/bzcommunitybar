2c2160ac2cc5fcae9de18ee010afaf4245054882
Brenna Clark <brennacclark@gmail.com>
2024-01-15 23:21:32 -0500

    Moving nav-brand to the center

--- src/components/views/nav/DesktopNav.tsx
+++ src/components/views/nav/DesktopNav.tsx
@@ -14,22 +14,18 @@ const DesktopNav: React.FC = () => {
   return (
     <header>
       <nav
-        className="navbar navbar-expand-md px-3 navbar-dark"
+        className="navbar navbar-expand-md navbar-dark"
         style={{
           backgroundColor: "var(--dark-bg-color)",
           color: "var(--dark-text-color)",
+          display: "flex",
+          justifyContent: "space-between", // Align the three sections
+          alignItems: "center",
+          padding: "0 10px",
         }}
       >
-        <div id="navbar-logo">
-          <img
-            className="mx-auto d-block bz-sign"
-            src="/Bz_Sign_Clean.png"
-            alt="B'z Community Bar Logo"
-            style={logoImgStyles}
-          />
-        </div>
-        <div id="navbar-links" className="container-fluid">
-          {/* Navbar Links */}
+        {/* Left Section - Menu and Events Links */}
+        <div className="d-flex align-items-center">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item px-2">
               <a href="#menu" className="nav-link">
@@ -42,36 +38,46 @@ const DesktopNav: React.FC = () => {
               </a>
             </li>
           </ul>
+        </div>
 
-          {/* Social Media Icons */}
-          <div className="social-icons-container d-flex">
-            <a
-              href="https://www.instagram.com/bz.communitybar"
-              target="_blank"
-              className="nav-link px-2"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/instagram.svg"
-                alt="Instagram"
-                className="social-icon"
-                style={socialIconStyles} // Apply the constant style
-              />
-            </a>
-            <a
-              href="https://www.facebook.com/profile.php?id=61553721551152"
-              target="_blank"
-              className="nav-link px-2"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="/facebook.svg"
-                alt="Facebook"
-                className="social-icon"
-                style={socialIconStyles} // Apply the constant style
-              />
-            </a>
-          </div>
+        {/* Center Section - Logo */}
+        <a href="#" className="navbar-brand">
+          <img
+            className="bz-sign"
+            src="/Bz_Sign_Clean.png"
+            alt="B'z Community Bar Logo"
+            style={logoImgStyles}
+          />
+        </a>
+
+        {/* Right Section - Social Media Icons */}
+        <div className="d-flex align-items-center">
+          <a
+            href="https://www.instagram.com/bz.communitybar"
+            target="_blank"
+            className="nav-link px-2"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/instagram.svg"
+              alt="Instagram"
+              className="social-icon"
+              style={socialIconStyles}
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=61553721551152"
+            target="_blank"
+            className="nav-link px-2"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/facebook.svg"
+              alt="Facebook"
+              className="social-icon"
+              style={socialIconStyles}
+            />
+          </a>
         </div>
       </nav>
     </header>
